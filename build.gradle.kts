8f12ed654f71b0e3807b8622dade139dfcb9d8c0
komamj <junmaokoma@gmail.com>
2020-12-16 14:44:15 +0800

    feat: [app] #N/A  add module.

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("maven-publish")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.2")
+
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+
+            isTestCoverageEnabled = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "consumer-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(Dependencies.Kotlin.STDLIB)
+
+    api(Dependencies.AndroidX.DATASTORE_PREFERENCES)
+
+    testImplementation(Dependencies.JunitTest.JUNIT)
+    testImplementation(Dependencies.JunitTest.TRUTH)
+    androidTestImplementation(Dependencies.AndroidTest.JUNIT)
+    androidTestImplementation(Dependencies.JunitTest.MOCK_CORE)
+    androidTestImplementation(Dependencies.JunitTest.TRUTH)
+    androidTestImplementation(Dependencies.AndroidTest.ESPRESSO_CORE)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components.findByName("release"))
+                groupId = "com.github.komamj"
+                artifactId = "platform-storage"
+                version = "0.0.1"
+            }
+        }
+    }
+}
